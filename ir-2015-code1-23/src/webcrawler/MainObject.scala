a81bfbb31e9581780d13de965379cf02b349c983
Ivaylo Toskov <kennelcrash@gmail.com>
2015-10-10 14:47:56 +0200

    Improved code quality. Added comments and refactored code snippets

--- ir-2015-code1-23/src/webcrawler/MainObject.scala
+++ ir-2015-code1-23/src/webcrawler/MainObject.scala
@@ -4,13 +4,11 @@ package webcrawler
  * @author Stefan
  */
 object MainObject {
-  def main(args: Array[String])
-  {
-    var linkArg : String = null
+  def main(args: Array[String]) {
+    var linkArg : String = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html"
     if(args.length > 0)
       linkArg = args.apply(0)
-    val crawler = new WebCrawlerManager()
+    val crawler = new WebCrawlerManager(linkArg)
     crawler.run()
-  
   }
 }
--- ir-2015-code1-23/src/webcrawler/WebCrawlerManager.scala
+++ ir-2015-code1-23/src/webcrawler/WebCrawlerManager.scala
@@ -1,71 +1,74 @@
 package webcrawler
 
-import io.Source
-import scala.util.matching.Regex
 import scala.collection.mutable.{Map => MutMap}
 
-class WebCrawlerManager(val baseUrl: String = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html") {
-  
+class WebCrawlerManager(val baseUrl: String) {
+  // A map that contains all of the links on the frontier
   var linkMap : MutMap[String,Boolean] = null
+  // A variable that denotes the domain limit which should not be leaved
   var domainLimit = ""
-  
-  def run(){
-    val(protocol, domain, tld) = LinkHelper.splitURL(baseUrl)
-    val baseDomain =  protocol + "://" + domain.substring(0, domain.lastIndexOf("/") + 1)
+
+  /**
+   * This is the entry point of the Crawler. The
+   * method takes care of the necessary initialization
+   * and starts the actual crawling.
+   */
+  def run() {
+    val domain = LinkHelper.splitURL(baseUrl)
     domainLimit = domain.substring(0, domain.lastIndexOf("www"))
-    println("base domain: " + domainLimit)
-    
-    linkMap = MutMap[String, Boolean]()  
+    println("base domain: " + domain)
+    println("base domainLimit: " + domainLimit)
+
+    linkMap = MutMap[String, Boolean]()
     linkMap += baseUrl -> false
-    
-    
-    //do initializations and other stuff
-    time {this.crawlEntry()}    
-  
+
+    time {this.crawlEntry()}
   }
-  
-  private def crawlEntry()
-  {
-    var linksRemaining = true
-    while(linksRemaining)
-    {
-      val nextLink = linkMap.find(p => p._2 == false).getOrElse(null)
-      if(nextLink!= null)
-      {
-      crawl(nextLink._1, nextLink._1.substring(0, nextLink._1.lastIndexOf("/") + 1))
-      }
-      else
-      {
-        linksRemaining = false
-      }
+
+  /**
+   * The method where the actual crawling happens.
+   * The method iterates through all the unvisited
+   * links and crawls through their content in the
+   * method crawl(url : String, domain: String).
+   */
+  private def crawlEntry() {
+    var nextLink = linkMap.find(!_._2).orNull
+    while(nextLink != null) {
+      crawl(nextLink._1)
+      nextLink = linkMap.find(!_._2).orNull
     }
-    //Number of links: 9576
+
     println("CRAWLER FINISHED")
   }
-  
-  //crawls recursively on all links
-  //entry point for all other processing
-  private def crawl(url : String, domain: String){
+
+  /**
+   * This method visits a single URL and processes its contents.
+   *
+   * @param url The base url to be visited and crawled
+   */
+  private def crawl(url : String) {
     linkMap(url) = true
     println("Current link: " + url)
-    //THIS IS WHERE YOU GET PAGE HTML
     val pageContents = LinkHelper.readUrlContent(url)
-    if(!(pageContents == null))
-    {
-    //extract href elements from html
-    LinkHelper.processLinks(LinkHelper.getLinks(pageContents), domain, linkMap, domainLimit)
-    //write links to file or something
-    println("Number of links: " + linkMap.size)
+    val domain = url.substring(0, url.lastIndexOf("/") + 1)
+    if(!(pageContents == null)) {
+      LinkHelper.processLinks(LinkHelper.getLinks(pageContents), domain, linkMap, domainLimit)
+      println("Number of links: " + linkMap.size)
     }
-    
   }
-  
+
+  /**
+   * A method that measures the time needed for the execution
+   * of a particular task.
+   * @param block The task whose execution time should be measured
+   * @tparam R Template parameter
+   * @return The result of the computation
+   */
   private def time[R](block: => R): R = {
     val t0 = System.nanoTime
-    val result = block    
+    val result = block
     val t1 = System.nanoTime
     println("Elapsed time: " + (t1 - t0) / 1000000000.0 + "s")
     result
-}
- 
+  }
 }
